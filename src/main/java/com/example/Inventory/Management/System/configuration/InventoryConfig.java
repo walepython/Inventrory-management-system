d351333a73e2ccc978e658ac9078739dec5bc8af
@waminisce <onidareadewaleoluwafemi69@gmail.com>
2026-07-03 13:39:33 +0100

    Update properties inventory-config

--- src/main/java/com/example/Inventory/Management/System/configuration/InventoryConfig.java
+++ src/main/java/com/example/Inventory/Management/System/configuration/InventoryConfig.java
@@ -76,7 +76,7 @@ public class InventoryConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173","https://soft-torrone-c5a63b.netlify.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
